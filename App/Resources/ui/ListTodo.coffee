27dc0c16c7b1dfb2d8cf3188bed9b524bdb63957
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2012-04-01 22:53:46 -0300

    Use navigation bar and open the details of Todo

--- App/Resources/ui/ListTodo.coffee
+++ App/Resources/ui/ListTodo.coffee
@@ -1,24 +1,29 @@
-Window = (user) ->
+Window = (user, nav) ->
 	
 	# Requires
-	xhr = require 'lib/xhr'
+	xhr = require "lib/xhr"
 	
 	# Create Window
 	self = Ti.UI.createWindow
-		title: 'Lista de tarefas'
+		title: "Lista de tarefas"
 	
 	# Create TableView
 	tableView = Ti.UI.createTableView()
 	self.add tableView
 
-	# Request to get Todo's list
-	xhr.request "GET", "http://techparty-todo-test.herokuapp.com/todo/{#user}", (response) ->
+	# Request to get Todo"s list
+	xhr.request "GET", "http://techparty-todo-test.herokuapp.com/todo/#{user}", (response) ->
 		
 		response = JSON.parse response
 
 		data = (task for task in response)
 
 		tableView.setData data
+
+	# Open the Todo
+	tableView.addEventListener "click", ->
+		todoView = require "ui/Todo"
+		nav.open todoView user, this._id, nav
 	
 	return self
 
--- App/Resources/ui/ListTodo.js
+++ App/Resources/ui/ListTodo.js
@@ -1,29 +1,32 @@
 (function() {
   var Window;
 
-  Window = function() {
+  Window = function(user, nav) {
     var self, tableView, xhr;
-    xhr = require('lib/xhr');
+    xhr = require("lib/xhr");
     self = Ti.UI.createWindow({
-      title: 'Lista de tarefas'
+      title: "Lista de tarefas"
     });
     tableView = Ti.UI.createTableView();
     self.add(tableView);
-    xhr.request("GET", "http://techparty-todo-test.herokuapp.com/todo", function(response) {
-      var data, t;
+    xhr.request("GET", "http://techparty-todo-test.herokuapp.com/todo/" + user, function(response) {
+      var data, task;
       response = JSON.parse(response);
       data = (function() {
         var _i, _len, _results;
         _results = [];
         for (_i = 0, _len = response.length; _i < _len; _i++) {
-          t = response[_i];
-          _results.push({
-            title: t.title
-          });
+          task = response[_i];
+          _results.push(task);
         }
         return _results;
       })();
       return tableView.setData(data);
+    });
+    tableView.addEventListener("click", function() {
+      var todoView;
+      todoView = require("ui/Todo");
+      return nav.open(todoView(user, this._id, nav));
     });
     return self;
   };
