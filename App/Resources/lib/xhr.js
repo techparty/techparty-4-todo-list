227ae6bfa1ccb003ffa336ad57087605cd2a815b
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2012-04-07 01:04:38 -0300

    Debug messages to requests

--- App/Resources/lib/xhr.js
+++ App/Resources/lib/xhr.js
@@ -2,11 +2,16 @@
 
   exports.request = function(type, url, callback, parameters) {
     var xhr;
+    Ti.API.info("Type: " + type);
+    Ti.API.info("URL: " + url);
+    Ti.API.info("Parameters: " + parameters);
     xhr = Ti.Network.createHTTPClient({
       onload: function() {
+        Ti.API.info("Response: " + this.responseText);
         return callback(this.responseText);
       },
       onerror: function() {
+        Ti.API.info("Error: " + this.responseText);
         return alert("Algum erro ocorreu em sua requisição");
       }
     });
